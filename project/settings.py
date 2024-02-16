1c068493dec16cef44d9c2e0226c1100cb081a55
jurie24102 <jurie24102@gmail.com>
2024-02-16 16:05:36 +0200

    django vercel

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1yf(3#*_ea9egphq*3dl+q&82^rw13mbov5#am4o110l@$nl$u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
